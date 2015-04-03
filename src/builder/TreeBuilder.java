85c9d11275327458273ea452d8fdf047450fbf06
Antoine Duchêne <antoine.duchene@student.uclouvain.be>
2015-04-03 22:15:20 +0200

    TreeBuilder --> forward

--- src/builder/TreeBuilder.java
+++ src/builder/TreeBuilder.java
@@ -10,6 +10,61 @@ import java.util.Stack;
  * Antoine Duchêne. 3/04/15
  */
 public class TreeBuilder implements TreeBuilderIF {
+
+    private class Node implements ExprIF {
+        private Node nextLeft;
+        private Node nextRight;
+        private Object content;
+
+        public Node(Object content, Node nextLeft, Node nextRight) {
+            this.content = content;
+            this.nextLeft = nextLeft;
+            this.nextRight = nextRight;
+        }
+
+        public Node(Object content) {
+            this.content = content;
+        }
+
+        public Node() {
+        }
+
+        public void setContent(Object content) {
+            this.content = content;
+        }
+
+        public void setNextLeft(Node nextLeft) {
+            this.nextLeft = nextLeft;
+        }
+
+        public void setNextRight(Node nextRight) {
+            this.nextRight = nextRight;
+        }
+
+        public Node getNextLeft() {
+            return nextLeft;
+        }
+
+        public Node getNextRight() {
+            return nextRight;
+        }
+
+        public Object getContent() {
+            return content;
+        }
+
+        @Override
+        public ExprIF getReducedTree() {
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            return null;
+        }
+    }
+
+    Node root;
     String exp;
 
     /**
@@ -23,9 +78,25 @@ public class TreeBuilder implements TreeBuilderIF {
 
     @Override
     public ExprIF build() {
-        Stack<String> stack;
-        //TODO Implémenter
-        return null;
+        Stack<Node> stack = new Stack<Node>();
+        for (int i = 0; i < exp.length() - 1; i++) {
+
+            if (isOperation(exp.charAt(i))) {
+                Node node = new Node(exp.charAt(i));
+                stack.push(node);
+            }
+
+            if (isClosingParenthesis(exp.charAt(i))) {
+                Node n = stack.pop();
+                Node n1 = stack.pop();
+                Node n2 = stack.pop();
+                n.setNextLeft(n1);
+                n.setNextRight(n2);
+                stack.push(n);
+            }
+
+        }
+        return stack.pop();
     }
 
     /**
@@ -43,7 +114,7 @@ public class TreeBuilder implements TreeBuilderIF {
     /**
      * @pre -
      * @post Return true si le caractère est ')'
-     *       False sinon
+     * False sinon
      */
     private static boolean isClosingParenthesis(char c) {
         if (c == ')') {
@@ -55,7 +126,7 @@ public class TreeBuilder implements TreeBuilderIF {
     /**
      * @pre -
      * @post Return true si le caractère est un chiffre
-     *        false sinon
+     * false sinon
      */
     private static boolean isDigit(char c) {
         if (c <= 9) {
