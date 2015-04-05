4135d8255fa14ef880f811f93f8c2cf61a16c58c
Antoine Duchêne <antoine.duchene@student.uclouvain.be>
2015-04-05 03:48:20 +0200

    Tests init

---
+++ src/UnitTests/ExprTest.java
@@ -0,0 +1,23 @@
+package UnitTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project : Algo_Projet_1
+ * Package : exprTree
+ * Antoine Duchêne. 5/04/15
+ */
+public class ExprTest {
+
+    @Test
+    public void testGetReducedTree() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
---
+++ src/UnitTests/TreeBuilderTest.java
@@ -0,0 +1,18 @@
+package UnitTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project : Algo_Projet_1
+ * Package : UnitTests
+ * Antoine Duchêne. 5/04/15
+ */
+public class TreeBuilderTest {
+
+    @Test
+    public void testBuild() throws Exception {
+
+    }
+}
